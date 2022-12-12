8fed22d2a7d718ab2f0f96e530724acfdae24eed
ivomar <ivomar.sci@gmail.com>
2022-12-12 19:04:13 -0300

    atualizacao

--- seeders/20221211141802-create-usuario.js
+++ seeders/20221211141802-create-usuario.js
@@ -3,18 +3,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
    await queryInterface.bulkInsert('usuario',[{
     nome: 'Ivomar Araújo de Carvalho',
     login: 'ivomar',
+    senha:'123',
+    chave:123,
+    situacao:1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+   },{
+    nome: 'Marcelo Marques dos Santos',
+    login: 'marcelo',
     senha:'123',
     chave:123,
     situacao:1,
